12c67ff7d32475a84d7da4027fadc3170cb69156
Peterblr <74207879+Peterblr@users.noreply.github.com>
2026-02-08 00:39:07 +0300

    created new component Map (#11)

---
+++ WebApp.Manager/Components/Map.razor.cs
@@ -0,0 +1,92 @@
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using System.Xml.Linq;
+using WebApp.Domain.Models;
+using WebApp.Manager.Store.Territory;
+
+namespace WebApp.Manager.Components;
+
+public partial class Map : ComponentBase
+{
+    [Parameter] public IEnumerable<TerritoryDto> Territories { get; set; } = Enumerable.Empty<TerritoryDto>();
+    [Parameter] public EventCallback<string> OnStateClick { get; set; }
+    [Inject] private IHttpClientFactory HttpClientFactory { get; set; } = default!;
+    [Inject] private IState<TerritoryState> State { get; set; } = default!;
+
+    protected string? SelectedCode { get; set; }
+    protected List<StateShapeDto> States { get; set; } = new();
+    protected bool Loading => State.Value.Loading;
+
+    protected StateShapeDto? SelectedState => States.FirstOrDefault(s => s.Code == SelectedCode);
+    protected override async Task OnInitializedAsync()
+    {
+        await PopulateMap();
+    }
+
+    private async Task PopulateMap()
+    {
+        var http = HttpClientFactory.CreateClient("AssetsClient");
+        var svg = await http.GetStringAsync("assets/us1.svg");
+
+        var xml = XDocument.Parse(svg);
+
+        var allStates = xml.Descendants()
+            .Where(x => x.Name.LocalName == "path")
+            .Select(x =>
+                {
+                    var code = (string?)x.Attribute("id") ?? "";
+                    var path = (string?)x.Attribute("d") ?? "";
+                    var name = (string?)x.Attribute("data-name") ?? "";
+
+                    var textElement = xml.Descendants()
+                        .FirstOrDefault(t => t.Name.LocalName == "text" &&
+                                             ((string?)t.Value)?.Trim().EndsWith(code, StringComparison.OrdinalIgnoreCase) == true);
+
+                    var textValue = textElement?.Value ?? "";
+                    var textX = (string?)textElement?.Attribute("x") ?? "0";
+                    var textY = (string?)textElement?.Attribute("y") ?? "0";
+                    var fontSize = (string?)textElement?.Attribute("font-size") ?? "12";
+
+                    return new StateShapeDto
+                    {
+                        Code = code,
+                        Path = path,
+                        Name = name,
+                        Text = textValue,
+                        TextX = textX,
+                        TextY = textY,
+                        FontSize = fontSize
+                    };
+                }
+            ).ToList();
+
+        var territoryCodes = Territories
+            .Select(t => t.Code)
+            .ToHashSet(StringComparer.OrdinalIgnoreCase);
+
+        States = allStates
+            .Where(s => territoryCodes.Contains(s.Code))
+            .ToList();
+    }
+
+    protected void OnClick(string id)
+    {
+        SelectedCode = id;
+        OnStateClick.InvokeAsync(id);
+    }
+
+    protected string GetFill(string code)
+    {
+        var t = Territories.FirstOrDefault(x => x.Code.Equals(code, StringComparison.OrdinalIgnoreCase));
+        if (t is null) return "#f9f9f9";
+
+        return t.RegionCode.ToLowerInvariant() switch
+        {
+            "west" => "#6cc070",
+            "south" => "#f7c24f",
+            "midwest" => "#4f8ef7",
+            "northeast" => "#b36cf0",
+            _ => "#f9f9f9"
+        };
+    }
+}
---
+++ WebApp.Manager/Pages/MostPopularCars.razor.cs
@@ -0,0 +1,37 @@
+﻿using Fluxor;
+using Microsoft.AspNetCore.Components;
+using WebApp.Domain.Models;
+using WebApp.Manager.Store.Territory;
+
+namespace WebApp.Manager.Pages;
+
+public partial class MostPopularCars
+{
+    [Inject] private IState<TerritoryState> State { get; set; } = default!;
+    [Inject] private IDispatcher Dispatcher { get; set; } = default!;
+
+    protected bool Loading => State.Value.Loading;
+    protected List<TerritoryDto> TerritoriesModel => State.Value.Territories;
+
+    protected override async Task OnInitializedAsync()
+    {
+        State.StateChanged += OnStateChanged;
+        Dispatcher.Dispatch(new LoadTerritoriesAction("us"));
+        await base.OnInitializedAsync();
+    }
+
+    private void HandleStateClick(string code)
+    {
+        Console.WriteLine($"Clicked state: {code}");
+    }
+
+    private void OnStateChanged(object? sender, EventArgs e)
+    {
+        InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        State.StateChanged -= OnStateChanged;
+    }
+}
