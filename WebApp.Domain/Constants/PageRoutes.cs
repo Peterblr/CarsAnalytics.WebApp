0b535d86590211604192d8a03d96c9ac9b437225
Peterblr <74207879+Peterblr@users.noreply.github.com>
2026-02-11 13:57:13 +0300

    Page most popular cars (#12)
    * created new component Map
    
    * added page routes class

---
+++ WebApp.Domain/Constants/PageRoutes.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApp.Domain.Constants;
+
+public static class PageRoutes
+{
+    public const string TerritoriesRoute = $"/territories";
+    public const string MostPopularCarsRoute = $"/most-popular-cars";
+}
